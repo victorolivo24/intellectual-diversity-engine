3eb76bbb55c7a304916162092276f89e8276da47
victorolivo24 <victormolivo3@gmail.com>
2025-07-19 16:16:03 -0400

    wrap App in Sentry error boundary

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5e67229e0c87552eb827f7a5cfcdfc4c@o4509696998506496.ingest.us.sentry.io/4509697044971520",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, sample the session when an error occurs.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred. Our team has been notified.</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
 
