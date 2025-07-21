050c75f34eb89c36640e3431e8db94d827b299e4
victorolivo24 <victormolivo3@gmail.com>
2025-07-21 16:31:59 -0400

    build: Add script to download NLTK data on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python download_nltk_data.py
+
 flask --app app db upgrade
---
+++ backend/download_nltk_data.py
@@ -0,0 +1,7 @@
+# In download_nltk_data.py
+import nltk
+
+print("--- Downloading NLTK data... ---")
+nltk.download("stopwords")
+nltk.download("wordnet")
+print("--- NLTK data downloaded successfully. ---")
